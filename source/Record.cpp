6c88a78ff2d0264d5a45745a57c2d03a60ed884a
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-04-24 20:46:01 +0900

    Proper dump for records

--- source/Record.cpp
+++ source/Record.cpp
@@ -93,18 +93,70 @@ void Record::dump(const int32_t level) const noexcept {
     std::cout << "    len  = " << level << std::endl;
     std::cout << "    data = ";
     switch(type_id) {
-      case 4:
-        for (size_t i = 0; i < (data_length / sizeof(int32_t)) && i < level; i++) {
+      case TA_TYPE_CHAR8:
+        for (size_t i = 0; i < (data_length / sizeof(char)) && i < (size_t)level; i++) {
+          std::cout << (char)(reinterpret_cast<char*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_INT8:
+        for (size_t i = 0; i < (data_length / sizeof(int8_t)) && i < (size_t)level; i++) {
+          std::cout << (int8_t)(reinterpret_cast<int8_t*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_INT16:
+        for (size_t i = 0; i < (data_length / sizeof(int16_t)) && i < (size_t)level; i++) {
+          std::cout << (int16_t)(reinterpret_cast<int16_t*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_INT32:
+        for (size_t i = 0; i < (data_length / sizeof(int32_t)) && i < (size_t)level; i++) {
           std::cout << (int32_t)(reinterpret_cast<int32_t*>(data))[i] << " ";
         }
         break;
-      default:
-        for (size_t i = 0; i < (data_length / sizeof(int8_t)) && i < level; i++) {
-          std::cout << std::hex << (int)reinterpret_cast<uint8_t*>(this->data)[i] << " ";
+      case TA_TYPE_INT64:
+        for (size_t i = 0; i < (data_length / sizeof(int64_t)) && i < (size_t)level; i++) {
+          std::cout << (int64_t)(reinterpret_cast<int64_t*>(data))[i] << " ";
         }
         break;
+      case TA_TYPE_UINT8:
+        for (size_t i = 0; i < (data_length / sizeof(uint8_t)) && i < (size_t)level; i++) {
+          std::cout << (uint8_t)(reinterpret_cast<uint8_t*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_UINT16:
+        for (size_t i = 0; i < (data_length / sizeof(uint16_t)) && i < (size_t)level; i++) {
+          std::cout << (uint16_t)(reinterpret_cast<uint16_t*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_UINT32:
+        for (size_t i = 0; i < (data_length / sizeof(uint32_t)) && i < (size_t)level; i++) {
+          std::cout << (uint32_t)(reinterpret_cast<uint32_t*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_UINT64:
+        for (size_t i = 0; i < (data_length / sizeof(uint64_t)) && i < (size_t)level; i++) {
+          std::cout << (uint64_t)(reinterpret_cast<uint64_t*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_REAL32:
+        for (size_t i = 0; i < (data_length / sizeof(float)) && i < (size_t)level; i++) {
+          std::cout << (float)(reinterpret_cast<float*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_REAL64:
+        for (size_t i = 0; i < (data_length / sizeof(double)) && i < (size_t)level; i++) {
+          std::cout << (double)(reinterpret_cast<double*>(data))[i] << " ";
+        }
+        break;
+      case TA_TYPE_UNKNOWN:
+      default:
+        for (size_t i = 0; i < (data_length / sizeof(int8_t)) && i < (size_t)level; i++) {
+          std::cout << std::hex << (int)reinterpret_cast<uint8_t*>(this->data)[i] << " ";
+        }
+        std::cout << std::dec;
+        break;
     }
-    std::cout << std::dec << std::endl;
+    std::cout << std::endl;
   }
 }
 
