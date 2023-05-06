ed5d876ad2e07dc096e31d1884976b8233bbd2e6
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-05-06 14:45:30 +0900

    Use static_cast instead reinterpret_cast in C_API; remove unused const qalifiers

--- source/C_API.cpp
+++ source/C_API.cpp
@@ -5,16 +5,16 @@
 using namespace tagarray;
 
 extern "C" void *new_container(const char *const comment) noexcept {
-  return reinterpret_cast<void *>(new Container(comment));
+  return static_cast<void *>(new Container(comment));
 }
 
 extern "C" int32_t get_container_status(const void *const container) noexcept {
-  return reinterpret_cast<const Container *const>(container)->get_status();
+  return static_cast<const Container *>(container)->get_status();
 }
 
 extern "C" void dump_container(const void *const container,
                                const int32_t level) noexcept {
-  reinterpret_cast<const Container *const>(container)->dump(level);
+  static_cast<const Container *>(container)->dump(level);
 }
 
 extern "C" void *load_container_c8(const char *const filename) noexcept {
@@ -27,16 +27,16 @@ extern "C" void *load_container_c32(const char32_t *const filename) noexcept {
 
 extern "C" void save_container_c8(void *const container,
                                   const char *const filename) noexcept {
-  reinterpret_cast<Container *const>(container)->save(filename);
+  static_cast<Container *>(container)->save(filename);
 }
 
 extern "C" void save_container_c32(void *const container,
                                    const char32_t *const filename) noexcept {
-  reinterpret_cast<Container *const>(container)->save(filename);
+  static_cast<Container *>(container)->save(filename);
 }
 
 extern "C" void delete_container(void *const container) noexcept {
-  delete reinterpret_cast<Container *const>(container);
+  delete static_cast<Container *>(container);
 }
 
 extern "C" void *new_record(const uint32_t type_id, const uint32_t n_dimensions,
@@ -45,39 +45,39 @@ extern "C" void *new_record(const uint32_t type_id, const uint32_t n_dimensions,
                             const uint64_t dimensions[TA_DIMENSIONS_LENGTH],
                             const int64_t options[TA_OPTIONS_LENGTH],
                             const char *const comment) noexcept {
-  return reinterpret_cast<void *>(new Record(
+  return static_cast<void *>(new Record(
       type_id, n_dimensions, data, data_length, dimensions, options, comment));
 }
 
 extern "C" int32_t get_record_status(const void *const record) noexcept {
-  return reinterpret_cast<const Record *const>(record)->get_status();
+  return static_cast<const Record *>(record)->get_status();
 }
 
 extern "C" void dump_record(const void *const record,
                             const int32_t level) noexcept {
-  reinterpret_cast<const Record *const>(record)->dump(level);
+  static_cast<const Record *>(record)->dump(level);
 }
 
 extern "C" void delete_record(void *const record) noexcept {
-  delete reinterpret_cast<Record *const>(record);
+  delete static_cast<Record *>(record);
 }
 
 extern "C" void add_record(void *const container, const char *const tag,
                            void *const record) noexcept {
-  reinterpret_cast<Container *const>(container)->add_record(
-      tag, *reinterpret_cast<Record *const>(record));
+  static_cast<Container *>(container)->add_record(
+      tag, *static_cast<Record *>(record));
 }
 
 extern "C" void *get_record(void *const container,
                             const char *const tag) noexcept {
-  return reinterpret_cast<Container *const>(container)->get_record(tag);
+  return static_cast<Container *>(container)->get_record(tag);
 }
 
 extern "C" void container_remove_record(void *const container,
                                         const char *const tag) noexcept {
-  return reinterpret_cast<Container *const>(container)->remove_record(tag);
+  return static_cast<Container *>(container)->remove_record(tag);
 }
 
 extern "C" RecordInfo get_record_info(const void *const record) noexcept {
-  return reinterpret_cast<const Record *const>(record)->get_info();
+  return static_cast<const Record *>(record)->get_info();
 }
