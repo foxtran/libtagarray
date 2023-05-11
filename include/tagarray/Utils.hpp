15d215e7004ed87ed6000b2dfb738bacb2ffa52a
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-05-11 19:40:59 +0900

    Inline for check_tag and check_ptr

--- include/tagarray/Utils.hpp
+++ include/tagarray/Utils.hpp
@@ -9,13 +9,13 @@ namespace tagarray {
 namespace utils {
 
 template <typename T>
-int32_t check_ptr(const T *const ptr) noexcept {
+inline int32_t check_ptr(const T *const ptr) noexcept {
   if (ptr == nullptr)
     return TA_NULLPTR;
   return TA_OK;
 }
 
-int32_t check_tag(const std::string &tag) noexcept {
+inline int32_t check_tag(const std::string &tag) noexcept {
   std::regex tag_regex("^\b[ -~]+\b$");
   if(std::regex_match(tag, tag_regex))
     return TA_OK;
