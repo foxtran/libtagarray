f501e4dd2a758776ad43b24f8ffec0eaf1932f11
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-05-12 05:06:51 +0900

    Improve check_tag

--- include/tagarray/Utils.hpp
+++ include/tagarray/Utils.hpp
@@ -1,23 +1,31 @@
 #pragma once
 
-#include <string>
 #include <regex>
+#include <string>
 
 #include "tagarray/defines.h"
 
 namespace tagarray {
 namespace utils {
 
-template <typename T>
-inline int32_t check_ptr(const T *const ptr) noexcept {
+template <typename T> inline int32_t check_ptr(const T *const ptr) noexcept {
   if (ptr == nullptr)
     return TA_NULLPTR;
   return TA_OK;
 }
 
 inline int32_t check_tag(const std::string &tag) noexcept {
-  std::regex tag_regex("^\b[ -~]+\b$");
-  if(std::regex_match(tag, tag_regex))
+  if (tag.size() == 0)
+    return TA_INCORRECT_TAG;
+  std::regex whitespace_regex("\\s");
+  std::string f(tag[0], 1);
+  std::string l(tag.back(), 1);
+  if (std::regex_match(f, whitespace_regex))
+    return TA_INCORRECT_TAG;
+  if (std::regex_match(l, whitespace_regex))
+    return TA_INCORRECT_TAG;
+  std::regex tag_regex("^[ -~]+$");
+  if (std::regex_match(tag, tag_regex))
     return TA_OK;
   return TA_INCORRECT_TAG;
 }
