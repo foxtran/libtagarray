c5156f950f3399c628df9bec288902be10bf730b
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-05-31 14:30:23 +0900

    Update tests

--- tests/Fortran/test-header-v1.F90
+++ tests/Fortran/test-header-v1.F90
@@ -6,6 +6,7 @@ program test
   use tagarray
   implicit none
   TEST(check_container)
+  TEST(check_record_value)
   TEST(check_record_data)
 contains
   integer function check_container() result(status)
@@ -47,7 +48,7 @@ contains
       status = 6
       return
     end if
-    TA_GET_CONTAINER_VALUE(container, "INT32-value", TA_TYPE_INT32, p_value, stat)
+    TA_CONTAINER_GET_VALUE(container, "INT32-value", TA_TYPE_INT32, p_value, stat)
     if (stat /= TA_OK) then
       status = 7
       return
@@ -56,7 +57,7 @@ contains
       status = 8
       return
     end if
-    TA_GET_CONTAINER_DATA(container, "INT32-array", TA_TYPE_INT32, p_array, stat)
+    TA_CONTAINER_GET_DATA(container, "INT32-array", TA_TYPE_INT32, p_array, stat)
     if (stat /= TA_OK) then
       status = 9
       return
@@ -65,7 +66,7 @@ contains
       status = 10
       return
     end if
-    TA_GET_CONTAINER_VALUE(container, "INT32-value-opt", TA_TYPE_INT32, p_value, stat)
+    TA_CONTAINER_GET_VALUE(container, "INT32-value-opt", TA_TYPE_INT32, p_value, stat)
     if (stat /= TA_OK) then
       status = 11
       return
@@ -79,7 +80,7 @@ contains
       status = 13
       return
     end if
-    TA_GET_CONTAINER_DATA(container, "INT32-array-opt", TA_TYPE_INT32, p_array, stat)
+    TA_CONTAINER_GET_DATA(container, "INT32-array-opt", TA_TYPE_INT32, p_array, stat)
     if (stat /= TA_OK) then
       status = 14
       return
@@ -96,6 +97,59 @@ contains
     call container%delete()
     status = 0
   end function check_container
+  integer function check_record_value() result(status)
+    use, intrinsic :: iso_c_binding, only: c_int32_t
+    type(record_t) :: record
+    integer(c_int32_t) :: stat
+    integer(c_int32_t), target, allocatable :: value
+    integer(c_int32_t), pointer :: p_value
+    status = -1
+    allocate(value, source = 1_c_int32_t)
+    TA_RECORD_NEW_VALUE(record, TA_TYPE_INT32, value)
+    stat = record%get_status()
+    if (stat /= TA_OK) then
+      status = 1
+      return
+    end if
+    TA_RECORD_GET_VALUE(record, TA_TYPE_INT32, p_value, stat)
+    if (stat /= TA_OK) then
+      status = 2
+      return
+    end if
+    if (value /= p_value) then
+      status = 3
+      return
+    end if
+    value = 2_c_int32_t
+    if (value == p_value) then
+      status = 4
+      return
+    end if
+    call record%delete()
+    deallocate(value)
+    allocate(value, source = 1_c_int32_t)
+    TA_RECORD_NEW_VALUE_OPTIONAL(record, TA_TYPE_INT32, value, comment = "INT32-array-opt")
+    stat = record%get_status()
+    if (stat /= TA_OK) then
+      status = 5
+      return
+    end if
+    TA_RECORD_GET_VALUE(record, TA_TYPE_INT32, p_value, stat)
+    if (stat /= TA_OK) then
+      status = 6
+      return
+    end if
+    if (value /= p_value) then
+      status = 7
+      return
+    end if
+    value = 2_c_int32_t
+    if (value == p_value) then
+      status = 8
+      return
+    end if
+    status = 0
+  end function check_record_value
   integer function check_record_data() result(status)
     use, intrinsic :: iso_c_binding, only: c_int32_t
     type(record_t) :: record
@@ -104,13 +158,13 @@ contains
     integer(c_int32_t), pointer :: p_array(:)
     status = -1
     allocate(array(10), source = 2_c_int32_t)
-    TA_RECORD_NEW(record, TA_TYPE_INT32, array)
+    TA_RECORD_NEW_DATA(record, TA_TYPE_INT32, array)
     stat = record%get_status()
     if (stat /= TA_OK) then
       status = 1
       return
     end if
-    TA_GET_RECORD_DATA(record, TA_TYPE_INT32, p_array, stat)
+    TA_RECORD_GET_DATA(record, TA_TYPE_INT32, p_array, stat)
     if (stat /= TA_OK) then
       status = 2
       return
@@ -127,13 +181,13 @@ contains
     call record%delete()
     deallocate(array)
     allocate(array(10), source = 2_c_int32_t)
-    TA_RECORD_NEW_OPTIONAL(record, TA_TYPE_INT32, array, comment = "INT32-array-opt")
+    TA_RECORD_NEW_DATA_OPTIONAL(record, TA_TYPE_INT32, array, comment = "INT32-array-opt")
     stat = record%get_status()
     if (stat /= TA_OK) then
       status = 5
       return
     end if
-    TA_GET_RECORD_DATA(record, TA_TYPE_INT32, p_array, stat)
+    TA_RECORD_GET_DATA(record, TA_TYPE_INT32, p_array, stat)
     if (stat /= TA_OK) then
       status = 6
       return
