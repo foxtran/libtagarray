5ac3233e9b670ca82b18792ed228e046f52a4515
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-12-07 21:45:25 +0100

    Default initialization of Crecordinfo

--- source/API/Fortran/Crecordinfo.f90
+++ source/API/Fortran/Crecordinfo.f90
@@ -1,16 +1,16 @@
 module tagarray_Crecordinfo
-  use, intrinsic :: iso_c_binding, only: c_ptr, c_int32_t, c_int64_t
+  use, intrinsic :: iso_c_binding, only: c_ptr, c_int32_t, c_int64_t, c_null_ptr
   use tagarray_defines
   implicit none
   private
   type, bind(C) :: C_RecordInfo_t
-    integer(c_int32_t) :: type_id
-    integer(c_int32_t) :: itemsize
-    integer(c_int64_t) :: count
-    integer(c_int64_t) :: ndims
-    type(c_ptr)        :: dims
-    type(c_ptr)        :: data
-    type(c_ptr)        :: description
+    integer(c_int32_t) :: type_id = 0
+    integer(c_int32_t) :: itemsize = 0
+    integer(c_int64_t) :: count = 0
+    integer(c_int64_t) :: ndims = 0
+    type(c_ptr)        :: dims = c_null_ptr
+    type(c_ptr)        :: data = c_null_ptr
+    type(c_ptr)        :: description = c_null_ptr
   end type C_RecordInfo_t
   public :: C_RecordInfo_t
 end module tagarray_Crecordinfo
