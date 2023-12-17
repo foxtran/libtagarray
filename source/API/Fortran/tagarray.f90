ab51c2bb53aa37e8e15cbca5d4ebd50dee5dda9f
Igor S. Gerasimov <i.s.ger@ya.ru>
2023-12-17 08:29:21 +0100

    Load more variables by default from iso_c_binding module

--- source/API/Fortran/tagarray.f90
+++ source/API/Fortran/tagarray.f90
@@ -1,5 +1,5 @@
 module tagarray
-  use, intrinsic :: iso_c_binding, only: c_f_pointer
+  use, intrinsic :: iso_c_binding, only: c_f_pointer, c_loc, c_int32_t, c_int64_t, c_bool
   use tagarray_defines
   use tagarray_utils
   use tagarray_recordinfo
